018551fb0fcb5ccc1db368a8deb690177e5b2e7a
kyle <elvis871229@gmail.com>
2024-05-02 18:39:31 +0800

    update

---
+++ client/src/views/login/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header class="header-wrapped">
+        <div class="header-content">
+          <h3>賴暄妮打嗝管理系統</h3>
+          <span class="welcome">一天到底要打幾次嗝</span>
+        </div>
+      </el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+//header外
+.header-wrapped {
+  //header內
+  .header-content {
+    width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .welcome {
+      font-size: 13px;
+    }
+  }
+}
+</style>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { tr } from "element-plus/es/locale/index.mjs";
+import { resolve } from "path";
+const pathResolve = (dir) => resolve(__dirname, dir);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +15,10 @@ export default defineConfig({
     port: 8080, //通訊Port號
     open: true, //自動默認開啟瀏覽器
     cors: true, //自動默認跨域
+  },
+  resolve: {
+    alias: {
+      "@": pathResolve("./src"),
+    },
   },
 });
